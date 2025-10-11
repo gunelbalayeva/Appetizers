1386d32737f535e1de509e025c14fdb047a22813
gunelbalayeva <gunelbalayeva97@gmail.com>
2025-10-11 17:52:16 +0400

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by User on 11.10.25.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
